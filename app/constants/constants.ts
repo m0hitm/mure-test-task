db91ee922128e358b686a756b93ffdfae762907e
Mohit Mishra <m0hit.mishra0301@gmail.com>
2024-02-09 01:39:35 +0530

    feat: add constants file

---
+++ app/constants/constants.ts
@@ -0,0 +1,6 @@
+export const ALCHEMY_KEY = "KPomeXmCrJnRQgGt1Mcga_PI0M8Olhuq";
+export const WALLET_CONNECT_PROJECT_ID = "814ca7dfadab91ac30a9c21b7dba0e4f";
+export const BASE_URL = "https://api.felixnorden.workers.dev/";
+export const SEPOLIA_RPC = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`;
+export const MURE_POOL_FACTORY_CONTRACT = `0x2081d63A4A664D388D61a09eAE0f6036A53Df83e`;
+export const API_KEY = `test`;
