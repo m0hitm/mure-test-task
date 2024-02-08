44b894cab11606fbbd5908b80416fcd1c4ea53d4
Mohit Mishra <m0hit.mishra0301@gmail.com>
2024-02-09 01:40:00 +0530

    feat: add get signature api route

---
+++ app/api/route.ts
@@ -0,0 +1,27 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { NextResponse } from "next/server";
+import axiosInstance from "../utils/axiosInstance";
+import { URL } from "url";
+
+export async function GET(request: NextApiRequest) {
+  const { searchParams } = new URL(request.url as string);
+  const appName = searchParams.get("appName");
+  const version = searchParams.get("version");
+  const appOwner = searchParams.get("appOwner");
+  const caller = searchParams.get("caller");
+  const chainId = searchParams.get("chainId");
+
+  try {
+    const resp = await axiosInstance.get(
+      `factory/signature?appName=${appName}&version=${version}&appOwner=${appOwner}&chainId=${chainId}&caller=${caller}`
+    );
+
+    return new NextResponse(JSON.stringify({ data: resp.data }), {
+      status: 200,
+    });
+  } catch (err) {
+    return new NextResponse(JSON.stringify({ err: err }), {
+      status: 400,
+    });
+  }
+}
