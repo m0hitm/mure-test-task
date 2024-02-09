3b72aec6abfa467717a28c254f1217b55cfc132e
Mohit Mishra <m0hit.mishra0301@gmail.com>
2024-02-09 22:58:45 +0530

    feat: add sepolia rpc for client

--- app/utils/walletClient.ts
+++ app/utils/walletClient.ts
@@ -5,20 +5,22 @@ import {
   createWalletClient,
   http,
 } from "viem";
+import { SEPOLIA_RPC } from "../constants/constants";
 
 export async function getWalletClient(chain: Chain) {
   const walletClient = createWalletClient({
     chain: chain,
-    transport: http(chain.rpcUrls.default.http[0]),
+    transport: http(SEPOLIA_RPC),
   });
 
-  return walletClient;
+  const [address] = await walletClient.getAddresses();
+  return { walletClient, address };
 }
 
 export async function getPublicClient(chain: Chain) {
   const publicClient = createPublicClient({
     chain: chain,
-    transport: http(chain.rpcUrls.default.http[0]),
+    transport: http(SEPOLIA_RPC),
   });
 
   return publicClient;
