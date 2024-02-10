4ec08d9dde324b6322b680a23f0e2ddcd804ef43
Mohit Mishra <m0hit.mishra0301@gmail.com>
2024-02-10 12:37:58 +0530

    feat: add envs

--- app/constants/constants.ts
+++ app/constants/constants.ts
@@ -1,6 +1,10 @@
-export const ALCHEMY_KEY = "KPomeXmCrJnRQgGt1Mcga_PI0M8Olhuq";
-export const WALLET_CONNECT_PROJECT_ID = "814ca7dfadab91ac30a9c21b7dba0e4f";
-export const BASE_URL = "https://api.felixnorden.workers.dev/";
-export const SEPOLIA_RPC = `https://eth-sepolia.g.alchemy.com/v2/${ALCHEMY_KEY}`;
-export const MURE_POOL_FACTORY_CONTRACT = `0x2081d63A4A664D388D61a09eAE0f6036A53Df83e`;
-export const API_KEY = `test`;
+import { Address } from "viem";
+
+export const ALCHEMY_KEY = process.env.NEXT_PUBLIC_ALCHEMY_ID;
+export const WALLET_CONNECT_PROJECT_ID =
+  process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const SEPOLIA_RPC = `${process.env.NEXT_PUBLIC_SEPOLIA_RPC}${ALCHEMY_KEY}`;
+export const MURE_POOL_FACTORY_CONTRACT =
+  process.env.NEXT_PUBLIC_MURE_POOL_FACTORY_CONTRACT;
+export const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
