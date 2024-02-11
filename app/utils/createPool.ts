7fa8f9cf3d078e8cfb80b4bedab627a77de10842
Mohit Mishra <m0hit.mishra0301@gmail.com>
2024-02-11 12:47:52 +0530

    feat: add parse logs logic

--- app/utils/createPool.ts
+++ app/utils/createPool.ts
@@ -2,7 +2,7 @@ import { getWalletClient, getPublicClient } from "./walletClient";
 import { CreatePoolProps } from "../types/types";
 import { MURE_POOL_FACTORY_CONTRACT } from "../constants/constants";
 import { MURE_POOL_FACTORY_ABI } from "../abis/MurePoolFactory";
-import { Address } from "viem";
+import { Address, parseEventLogs } from "viem";
 export default async function createPool(props: CreatePoolProps) {
   const { chain, appName, version, appOwner, deadline, sig, data } = props;
 
@@ -16,8 +16,25 @@ export default async function createPool(props: CreatePoolProps) {
     functionName: "deployAppProxy",
     args: [appName, version, appOwner, deadline, data, sig],
   });
-  
+
   const txHash = await walletClient.writeContract(request);
-  const proxy = await publicClient.waitForTransactionReceipt({ hash: txHash });
-  return proxy;
+  const txRecipt = await publicClient.waitForTransactionReceipt({
+    hash: txHash,
+  });
+
+  const logs = parseEventLogs({
+    abi: MURE_POOL_FACTORY_ABI,
+    logs: txRecipt.logs,
+    eventName: "Deployment",
+  });
+
+  const appAddress = logs[0].args.appAddress;
+  const deployedAppAddresses = JSON.parse(
+    localStorage.getItem("appAddresses") || "[]"
+  );
+
+  deployedAppAddresses.push(appAddress);
+  localStorage.setItem("appAddresses", JSON.stringify(deployedAppAddresses));
+
+  return appAddress;
 }
